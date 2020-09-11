fbed40dc01da894e71a0c04ac2e1f10f8190c582
Luke Carapezza <me@lukec.me>
2020-09-11 13:32:01 -0400

    Automate GitHub flow (#3)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import { request } from '@octokit/request'
+import Airtable from 'airtable'
+import fetch from 'node-fetch'
+
 
 const env = process.env.NODE_ENV || 'development'
 if (env === 'development') {
   require('dotenv').config()
 }
+
+
+/* Configure Airtable */
+Airtable.configure({
+  apiKey: process.env.AIRTABLE_API_KEY
+})
+
+const SDP_BASE_ID = 'apple9fiV81JsRytC' // Base ID for "SDP Priority Activations" base
+const base = Airtable.base(SDP_BASE_ID);
 
 const app = express()
 
@@ -56,7 +68,7 @@ const objectToQueryString = obj => {
 }
 
 app.get('/hack-pack', async (req, res) => {
-  let destinationUrl = 'https://airtable.com/shrNMxeoANyxtVY8U'
+  let destinationUrl = 'https://airtable.com/shrQGYvcSqhta7xk6'
   try {
     const authData = await ghAuth(req)
 
@@ -73,22 +85,58 @@ app.get('/hack-pack', async (req, res) => {
   }
 })
 
-app.get('/hack-pack2', async (req, res) => {
-  let destinationUrl = 'https://airtable.com/shrQGYvcSqhta7xk6'
+app.get('/generate-account', async (req, res) => {
   try {
-    const authData = await ghAuth(req)
+    const recordId = req.query.recordId;
+    /* Check if person is already there */
 
-    const { login: username = '', email = '' } = authData.user
+    // Get user record
+    const record = base('SDP Priority Activations').find(recordId);
+    const userName = (await record).get('GitHub Username')
+    const status = (await record).get('Approved')
 
-    destinationUrl += objectToQueryString({
-      'prefill_GitHub Username': username,
-      'prefill_GitHub Email': email
-    })
-  } catch (e) {
-    console.error(e)
-  } finally {
-    res.redirect(302, destinationUrl)
-  }
+    // If they have used the URL already, send them to pack.
+
+    if (status === true){
+      res.redirect(302, 'https://education.github.com/pack')
+      return
+    }
+
+    const isApproved = await base('SDP Priority Activations').select({
+      filterByFormula: `AND({GitHub Username} = "${userName}", {Approved} = TRUE(), {Years Since} <= 2)`
+    }).all()
+
+    // Check if there isn't a previous record
+    if (isApproved.length !== 0) {
+      console.log('Previous record found');
+      res.redirect(302, 'https://education.github.com/pack') // They already had one, so send them there!
+      /* Set not approved for duplicate */
+      await base('SDP Priority Activations').update([{
+        id: recordId,
+        fields: {
+          'Rejection Reason': 'Duplicate Airtable submission'
+        }
+      }])
+      return;
+    }
+
+    /* Update record for "Approved" */
+    await base('SDP Priority Activations').update([{
+      id: recordId,
+      fields: {
+        'Approved': true
+      }
+    }])
+
+
+    /* Generate special link */
+    const activateRequest = await fetch(`https://education.github.com/student/verify/generate?school_id=27876&student_id=${recordId}&secret_key=${process.env.GITHUB_EDU_SECRET}`)
+
+    /* Redirect to special link */
+    res.redirect(302, await activateRequest.text());
+} catch (err) {
+  console.error(err);
+}
 })
 
 app.get('/dinoissour-badge', async(req, res) => {
